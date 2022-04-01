9385aece4564361cbea1c2fe69d7ba502f170667
Bao Truong <bao.truong@infonam.com>
2022-04-01 10:47:50 +0700

    shadow for box

--- src/pages/infouser/Infouser.tsx
+++ src/pages/infouser/Infouser.tsx
@@ -5,7 +5,7 @@ export default function Infouser() {
     <div id="infouser" className=" mx-auto px-8 py-12 text-base">
       {' '}
       <div className="w-1/2  mx-auto bg-zinc-300 shadow overflow-hidden sm:rounded-lg ">
-        <div className="px-4 py-5 sm:px-6">
+        <div className="px-4 py-5 sm:px-6 shadow-inner">
           <h3 className="text-lg leading-6 font-medium text-gray-900 ">Applicant Information</h3>
           <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
         </div>
