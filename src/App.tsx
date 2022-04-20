b3810f6074fc761e8c62f12f96ed6ae98abd1565
Lacia <phibang7899@gmail.com>
2022-04-20 18:03:48 +0700

    Fix 9.3

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import AdminTemplate from "templates/adminTemplate/AdminTempalte"
 import Cart from 'features/Cart/components/Cart';
 import Categories from 'features/Categories/components/Categories';
 import Detail from 'features/Detail/components/Detail';
-// import DetailTemplate from './templates/detailTemplate/DetailTemplate'
+import DetailTemplate from './templates/detailTemplate/DetailTemplate'
 import HistoryPay from 'features/HistoryPay/components/HistoryPay';
 import Home from './features/Home/Home';
 import HomeAdmin from "./Admin/Home/components/HomeAdmin"
@@ -26,13 +26,13 @@ function App() {
       <HomeTemplate exact path="/" Component={Home}></HomeTemplate>
       <HomeTemplate exact path="/home" Component={Home}></HomeTemplate>
       <HomeTemplate exact path="/about" Component={About}></HomeTemplate>
-      {/* <DetailTemplate exact path="/profile" Component={Infouser}></DetailTemplate>
+      <DetailTemplate exact path="/profile" Component={Infouser}></DetailTemplate>
       <DetailTemplate exact path="/carts" Component={Cart}></DetailTemplate>
       <DetailTemplate exact path="/likes" Component={Likes}></DetailTemplate>
       <DetailTemplate exact path="/history" Component={HistoryPay}></DetailTemplate>
       <DetailTemplate exact path="/detail/:id" Component={Detail}></DetailTemplate>
       <DetailTemplate exact path="/products" Component={Productlist}></DetailTemplate>
-      <DetailTemplate exact path="/categories/:categories" Component={Categories}></DetailTemplate> */}
+      <DetailTemplate exact path="/categories/:categories" Component={Categories}></DetailTemplate>
       {/* <AdminTemplate exact path="/admin/" Component={HomeAdmin}></AdminTemplate> */}
       <Route path="/login" exact component={Login} />
       <Route path="/signup" exact component={SignUp} />
